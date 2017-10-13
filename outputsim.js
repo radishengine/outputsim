1a7d4980b80b28e377c6216e73a0bcbd8d8e4015
radishengine <radishengine@gmail.com>
2017-10-14 00:22:03 +0100

    Update outputsim.js

--- outputsim.js
+++ outputsim.js
@@ -2,10 +2,53 @@
 self.outputsim = {};
 
 outputsim.init = function init(container) {
-  if (container.classList.contains('zx-spectrum')) {
-    return Promise.resolve(container);
-  }
-  else {
-    return Promise.reject('invalid or unspecified target');
-  }
+  return new Promise(function(resolve, reject) {
+    if (container.classList.contains('zx-spectrum')) {
+      const systemMemoryBufferSize = 128 * 1024;
+      const systemMemoryPageOffset = 0;
+      const systemMemoryPageSize = Math.ceil(systemMemoryBufferSize / (128 * 1024));
+      const viewportWidth = 256;
+      const viewportHeight = 192;
+      const borderWidth = 48;
+      const borderHeight = 48;
+      const totalWidth = borderWidth + viewportWidth + borderWidth;
+      const totalHeight = borderHeight + viewportHeight + borderHeight;
+      const pixelBufferSize = 4 * totalWidth * totalHeight;
+      const paletteBufferSize = 256 * 4;
+      const imageMemoryPageOffset = systemMemoryPageOffset + systemMemoryPageSize;
+      const imageMemoryPageSize = Math.ceil((pixelBufferSize + paletteBufferSize) / (128 * 1024));
+      container.memory = new WebAssembly.Memory({
+        initial: systemMemoryPageSize + imageMemoryPageSize,
+      });
+      container.palette = new Uint32Array(
+        container.memory.buffer,
+        imageMemoryPageOffset * (128 * 1024) + pixelBufferSize,
+        256);
+      const imageData = container.imageData = new ImageData(
+        new Uint8ClampedArray(
+          container.memory.buffer,
+          imageMemoryPageOffset * (128 * 1024),
+          pixelBufferSize),
+        totalWidth, totalHeight);
+      while (container.firstChild) container.removeChild(container.firstChild);
+      container.appendChild(container.canvas = document.createElement('CANVAS'));
+      const ctx2d = container.ctx2d = container.canvas.getContext('2d');
+      container.canvas.width = totalWidth;
+      container.canvas.height = totalHeight;
+      var nextFrameAt = performance.now();
+      function nextFrame(t) {
+        if (t >= nextFrameAt) {
+          nextFrameAt += 1/50;
+          if (nextFrameAt <= t) nextFrameAt = t + 1/50;
+          ctx2d.putImageData(imageData, 0, 0);
+        }
+        requestAnimationFrame(nextFrame);
+      }
+      nextFrame(nextFrameAt);
+      return container;
+    }
+    else {
+      return Promise.reject('invalid or unspecified target');
+    }
+  });
 };
